e069a4618baee563150aa4eca1eb98f0d347a89d
didinele <didinele.dev@gmail.com>
2022-08-17 21:51:12 +0300

    feat: compose script

---
+++ compose.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker-compose \
+  -p chatsift \
+  -f docker-compose.ama.yml \
+  -f docker-compose.automoderator.yml \
+  -f docker-compose.modmail.yml \
+  -f docker-compose.shared.yml \
+  -f docker-compose.website.yml \
+  ${@%$0}
